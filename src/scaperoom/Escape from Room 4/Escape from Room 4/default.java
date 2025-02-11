49f65f9952d6ebc825ff6cda392781c5f6149f4a
Marc Manoel Marinho de Alcantara N <marc.n@la.samsungsds.net>
2025-02-11 18:33:50 -0300

    Adding exam documentation

---
+++ src/scaperoom/Escape from Room 4/Escape from Room 4/default.java
@@ -0,0 +1,28 @@
+﻿////////
+// You can add and modify values and implementations if needed.
+// SW검정에서는 입출력 코드를 '문제의 조건에 맞게' 직접 작성하셔야 합니다.
+// 입출력은 표준 입출력을 사용하여야 합니다.
+// 단, 여러분의 PC에서 작업하실 때 필요한 System.setIn 구문은 아래 제시된 것을 이용하시면 됩니다.
+// 입력은 Scanner 를 사용하는 것을 권장합니다. 
+// 예) 한 줄에 있는 int형 정수 N 개를 입력받는 경우 (공백 구분)
+//     Scanner sc = new Scanner(System.in); // 최초 1회 설정
+//     ...
+//     for(int i=0; i<N; i++) myvalue[i] = sc.nextInt();
+////////
+import java.io.*;
+
+class Solution {
+	public static void main(String args[]) throws Exception {
+    	
+		// 아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 sample_input.txt 파일로부터
+		// 읽어오겠다는 의미의 코드입니다. 여러분이 작성한 코드를 테스트 할 때, 편의를 위해서
+		// sample_input.txt에 입력을 저장한 후, 이 코드를 프로그램의 처음 부분에 추가하면 
+		// 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+		// 따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
+		// 단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석처리 하셔야 합니다.
+		//System.setIn(new FileInputStream("sample_input.txt"));
+
+		//////// Your algorithm is implemented here. ////////
+
+	}
+}
---
+++ src/scaperoom/Escape from Room 4/Escape from Room 4/default_eng.java
@@ -0,0 +1,28 @@
+////////
+// You can add and modify values and implementations if needed.
+// Beginning with the SW test, you must directly write your input and output code according to the conditions of each question. 
+// You must use standard input and output. 
+// You may use necessary system.setIn syntax provided as below, while working on your desktop.  
+// For Input, using Scanner is recommended.  
+// e.g.) When N number of int type integers in a row are input (with a space in between)
+//     Scanner sc = new Scanner(System.in); // set one time at the very beginning
+//     ...
+//     for(int i=0; i<N; i++) myvalue[i] = sc.nextInt();
+////////
+import java.io.*;
+
+class Solution {
+	public static void main(String args[]) throws Exception {
+    	
+		// The method below means that the program will read from input file, 
+		// instead of standard(keyboard) input.
+		// To test your program, you may save input data in input file,
+		// and call below method to read from the file when using readLine() method.
+		// You may remove the comment symbols(//) in the below statement and use it.
+		// But before submission, you must remove the System.setIn method or rewrite comment symbols(//).
+		//System.setIn(new FileInputStream("sample_input.txt"));
+
+		//////// Your algorithm is implemented here. ////////
+
+	}
+}
